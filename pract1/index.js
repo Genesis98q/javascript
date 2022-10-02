64d61650b8ed50754ea4a9620814f42f2bca7598
Genesis98q <114840097+Genesis98q@users.noreply.github.com>
2022-10-01 21:29:12 -0500

    practica 1 sencillo

---
+++ pract1/index.js
@@ -0,0 +1,33 @@
+console.log('hola')
+let yo_me_llamo_gene="soy genesis"
+console.log(yo_me_llamo_gene)
+let numero1= 10
+console.log(numero1)
+let llena_de_mierdq=true
+console.log(llena_de_mierdq)
+let x,y,z; let si; 
+x=5; y="tele"; z=20
+console.log(y)
+
+let suma1, mas2, igual5
+suma1 = 3; mas2 = 2; igual5 = suma1 + mas2 
+console.log(igual5)
+let yo_me_llene, de_comida, ayudame_calderon
+yo_me_llene ='tacobell ' ; de_comida = 'y comida de casa'; ayudame_calderon = yo_me_llene + de_comida
+console.log(ayudame_calderon)
+document.getElementById('prueba').innerHTML = ayudame_calderon
+let amor  ;
+var odio ;
+amor = 'rosado ' 
+odio = "azul"
+let amistad = amor + odio 
+console.log(amistad)
+let vamosamultiplicar = 3
+let siyque = 4
+var yesyes = vamosamultiplicar * siyque
+console.log(yesyes)
+document.getElementById('multiplicacion1').innerHTML = yesyes
+var $hola = 'si'
+var _como = 'no' // diferentes formas de poner variables
+var Hola = 'gracias'
+document.getElementById('titulo').innerHTML = Hola
